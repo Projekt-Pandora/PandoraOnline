01017c1fc1230a55d0e0098db54aa2c70de046d4
DerColaTrinker <info@toolwheel.de>
2024-11-28 16:50:49 +0100

    Storage World Projekt und Migration eingefügt.

---
+++ libraries/Pandora.Server.Storage.World/src/Server/Storage/World/Migrations/M_0001.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pandora.Server.Storage.World.Migrations
+{
+    [Migration(1)]
+    public class M_0001 : Migration
+    {
+        public override void Down()
+        {
+            
+        }
+
+        public override void Up()
+        {
+           
+        }
+    }
+}
